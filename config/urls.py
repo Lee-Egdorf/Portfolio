e1772de69209543e147bf894340236e8ead1391d
Lee-Egdorf <legdorf@basecampcodingacademy.org>
2023-03-01 23:57:47 -0600

    version 1.1

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("skills", views.skills, name="skills"),
+    path("https://lee-egdorf.github.io/Portfolio/", views.index, name="index"),
+    path("skills/", views.skills, name="skills"),
     path("admin/", admin.site.urls),
 ]
